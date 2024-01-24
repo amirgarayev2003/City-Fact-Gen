e0f28d3d6080dac073d843301ead346e5d001aee
Amir Garayev <amirgarayevbusiness@gmail.com>
2024-01-24 13:36:27 +0000

    Replaced Selenium with BeautifulSoup.
    Fixed <p> tags not being passed through.

--- scraper-app.py
+++ scraper-app.py
@@ -1,6 +1,15 @@
-import scrapy
+import requests
+from bs4 import BeautifulSoup
+
+wiki_page = requests.get('https://en.wikipedia.org/wiki/Reading,_Berkshire')
 
 
-class QuoteSpider(scrapy.Spider):
-    name = 'quote-spdier'
-    start_urls = ['https://quotes.toscrape.com']
+# HTML parser 
+wiki_page = BeautifulSoup(wiki_page.content, 'html.parser')
+
+
+# Find p tags inside wiki body containers 
+
+wiki_page_body = wiki_page.find(id="bodyContent").find_all("p")
+
+print(wiki_page_body)
